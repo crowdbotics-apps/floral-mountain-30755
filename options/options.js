f7ff9ef823316a8db5670a68065c1f8bcb4a1594
Crowdbotics <team@crowdbotics.com>
2021-09-22 19:17:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "floral_mountain_30755Identifier",
+  url: "https://floral_mountain_30755Identifier.botics.co",
+  api: "https://floral_mountain_30755Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
